3a57c14e9bfeb397e19a47d01af0dcc2cf46993f
yungege <273461474@qq.com>
2020-12-27 14:45:20 +0800

    web服务器，WEBSOCKET服务器

---
+++ service/main.lua
@@ -0,0 +1,9 @@
+local skynet = require "skynet"
+
+skynet.start(function()
+    skynet.error("start begin...")
+	
+	skynet.newservice("http_server")
+	
+	skynet.newservice("websocket_server")
+end)
---
+++ service/websocket_worker/main.lua
@@ -0,0 +1,52 @@
+local skynet = require "skynet"
+local socket = require "skynet.socket"
+local service = require "skynet.service"
+local websocket = require "http.websocket"
+
+local handle = {}
+
+function handle.connect(id)
+	print("ws connect from: " .. tostring(id))
+end
+
+function handle.handshake(id, header, url)
+	local addr = websocket.addrinfo(id)
+	print("ws handshake from: " .. tostring(id), "url", url, "addr:", addr)
+	print("----header-----")
+	for k,v in pairs(header) do
+		print(k,v)
+	end
+	print("--------------")
+	websocket.write(id, "go")
+end
+
+function handle.message(id, msg, msg_type)
+	assert(msg_type == "binary" or msg_type == "text")
+	skynet.error(msg_type.." "..msg)
+	websocket.write(id, msg)
+end
+
+function handle.ping(id)
+	print("ws ping from: " .. tostring(id) .. "\n")
+end
+
+function handle.pong(id)
+	print("ws pong from: " .. tostring(id))
+end
+
+function handle.close(id, code, reason)
+	print("ws close from: " .. tostring(id), code, reason)
+end
+
+function handle.error(id)
+	print("ws error from: " .. tostring(id))
+end
+
+skynet.start(function ()
+	skynet.dispatch("lua", function (_,_, id, protocol, addr)
+		local ok, err = websocket.accept(id, handle, protocol, addr)
+		if not ok then
+			print(err)
+		end
+	end)
+end)
